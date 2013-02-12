35927e9bc31896c0824e71088fb09b28514e98f4
Mohamed F. Soliman <Mohamed.Soliman@emc.com>
2013-02-11 21:04:07 -0800

    Test cases: Query using bitmap and with function based bitmap indexes does not process additional WHERE condition JIRA:MPP-19058 [JIRA: GPSQL-1199] Merge PARISTX-ORCA into HAWQ-MAIN
    [git-p4: depot-paths = "//cdb2/feature/42x/QP/": change = 153159]

--- cdb-pg/src/test/regress/sql/partition_pruning_with_fn.sql
+++ cdb-pg/src/test/regress/sql/partition_pruning_with_fn.sql
@@ -56,3 +56,141 @@ explain select * from ds_4 where month_id::int in (200801, 1,55,6,6,6,6,66,565,6
 
 -- cleanup
 drop table ds_4;
+
+
+--
+-- See MPP-18979
+--
+
+CREATE TABLE ds_2
+(
+  month_id character varying(6),
+  cust_group_acc numeric(10),
+  mobile_no character varying(10),
+  source character varying(12),
+  vas_group numeric(10),
+  vas_type numeric(10),
+  count_vas integer,
+  amt_vas numeric(10,2),
+  network_type character varying(3),
+  execution_id integer
+)
+WITH (
+  OIDS=FALSE
+)
+DISTRIBUTED BY (cust_group_acc, mobile_no)
+PARTITION BY LIST(month_id)
+          (
+          PARTITION p200800 VALUES('200800'),
+          PARTITION p200801 VALUES('200801'),
+          PARTITION p200802 VALUES('200802'),
+          PARTITION p200803 VALUES('200803'),
+          PARTITION p200804 VALUES('200804'),
+          PARTITION p200805 VALUES('200805'),
+          PARTITION p200806 VALUES('200806'),
+          PARTITION p200807 VALUES('200807'),
+          PARTITION p200808 VALUES('200808'),
+          PARTITION p200809 VALUES('200809')
+);
+
+insert into ds_2(month_id) values('200800');
+insert into ds_2(month_id) values('200801');
+insert into ds_2(month_id) values('200802');
+insert into ds_2(month_id) values('200803');
+insert into ds_2(month_id) values('200804');
+insert into ds_2(month_id) values('200805');
+insert into ds_2(month_id) values('200806');
+insert into ds_2(month_id) values('200807');
+insert into ds_2(month_id) values('200808');
+insert into ds_2(month_id) values('200809');
+
+-- queries without bitmap scan
+explain select * from ds_2 where month_id::int in (200808, 1315) order by month_id;
+
+explain  select * from ds_2 where month_id::int in (200808, 200801, 2008010) order by month_id;
+
+select * from ds_2 where month_id::int in (200907, 1315) order by month_id;
+
+select * from ds_2 where month_id::int in (200808, 1315) order by month_id;
+
+select * from ds_2 where month_id::int in (200808, 200801) order by month_id;
+
+select * from ds_2 where month_id::int in (200808, 200801, 2008010) order by month_id;
+
+-- cleanup
+drop table ds_2;
+
+Create or replace function public.reverse(text) Returns text as $BODY$
+DECLARE
+   Original alias for $1;
+   Reverse_str text;
+   I int4;
+BEGIN
+   Reverse_str :='';
+   For I in reverse length(original)..1 LOOP
+   Reverse_str := reverse_str || substr(original,I,1);
+END LOOP;
+RETURN reverse_str;
+END;
+$BODY$ LANGUAGE plpgsql IMMUTABLE;
+
+drop table if exists dnsdata cascade;
+
+CREATE TABLE dnsdata(dnsname text) DISTRIBUTED RANDOMLY;
+
+CREATE INDEX dnsdata_d1_idx ON dnsdata USING bitmap (split_part(reverse(dnsname),'.'::text,1));
+
+
+CREATE INDEX dnsdata_d2_idx ON dnsdata USING bitmap (split_part(reverse(dnsname),'.'::text,2));
+
+insert into dnsdata values('www.google.com');
+insert into dnsdata values('www.google1.com');
+insert into dnsdata values('1.google.com');
+insert into dnsdata values('2.google.com');
+insert into dnsdata select 'www.b.com' from generate_series(1, 100000) as x(a);
+
+analyze dnsdata;
+
+-- queries with bitmap scan enabled
+set enable_bitmapscan=on;
+set enable_indexscan=on;
+set enable_seqscan=off;
+
+Select dnsname from dnsdata
+where (split_part(reverse('cache.google.com'),'.',1))=(split_part(reverse(dnsname),'.',1))
+and (split_part(reverse('cache.google.com'),'.',2))=(split_part(reverse(dnsname),'.',2)) 
+order by dnsname;
+
+Select dnsname from dnsdata
+where (split_part(reverse('cache.google.com'),'.',1))=(split_part(reverse(dnsname),'.',1))
+and (split_part(reverse('cache.google.com'),'.',2))=(split_part(reverse(dnsname),'.',2))
+and dnsname = 'cache.google.com'
+order by dnsname;
+
+-- cleanup
+drop table dnsdata cascade;
+drop function public.reverse(text) cascade;
+
+
+Create or replace function public.MyFunc(int) Returns text as $BODY$
+BEGIN
+  RETURN 0;
+END;
+$BODY$ LANGUAGE plpgsql IMMUTABLE;
+
+drop table if exists mytable cascade;
+
+create table mytable(i int, j int);
+insert into mytable select x, x+1 from generate_series(1, 100000) as x;
+analyze mytable;
+
+CREATE INDEX mytable_idx1 ON mytable USING bitmap(myfunc(i));
+
+
+select * from mytable where MyFunc(i)=0 and i=100 order by i;
+
+select * from mytable where MyFunc(i)=0 and i=-1 order by i;
+
+-- cleanup
+drop function MyFunc(int) cascade;
+drop table mytable cascade;
