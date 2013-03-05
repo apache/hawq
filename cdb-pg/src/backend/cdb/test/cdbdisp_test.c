ee2c05afcdc4fb315b6a6e21812f6b0cf18f69d8
Carlos Garcia Alvarado <Carlos.GarciaAlvarado@emc.com>
2013-03-05 14:55:35 -0800

    Unit test for fix for MPP-19171 when the size of the plan exceeds the size of an integer.
    JIRA:MPP-19171
    [JIRA: GPSQL-1199] Merge PARISTX-ORCA into HAWQ-MAIN
    [CR:CR-6902]
    
    [git-p4: depot-paths = "//cdb2/feature/42x/QP/": change = 155904]

---
+++ cdb-pg/src/backend/cdb/test/cdbdisp_test.c
@@ -0,0 +1,110 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include "cmockery.h"
+
+#include "c.h"
+#include "../cdbdisp.c"
+
+/* 
+ * Expected variables for Assertions 
+ */
+void
+_ExceptionalCondition()
+{
+	expect_any(ExceptionalCondition,conditionName);
+	expect_any(ExceptionalCondition,errorType);
+	expect_any(ExceptionalCondition,fileName);
+	expect_any(ExceptionalCondition,lineNumber);
+	will_be_called(ExceptionalCondition);
+}
+
+/*
+ * Mocked object initializations required for dispatchPlan.
+ */
+void
+_init_cdbdisp_dispatchPlan(QueryDesc *queryDesc)
+{
+	_ExceptionalCondition( );
+	_ExceptionalCondition( );
+
+	queryDesc->estate = (struct EState *)palloc0(sizeof(struct EState));
+	queryDesc->estate->es_sliceTable = (struct SliceTable *)
+                                            palloc0(sizeof(struct SliceTable));
+	queryDesc->operation = CMD_NOTHING;
+	queryDesc->plannedstmt = (PlannedStmt *)palloc0(sizeof(PlannedStmt));   
+
+	will_be_called(clear_relsize_cache);
+
+	expect_any(RootSliceIndex, estate);
+	will_return(RootSliceIndex,0);  
+}
+
+/* 
+ * Tests that cdbdisp_dispatchPlan handles a plan size overflow 
+ * when splan_len_uncompressed * num_slices.
+ */
+void
+test__cdbdisp_dispatchPlan__Overflow_plan_size_in_kb(void **state)
+{
+	bool success = false;
+
+	struct CdbDispatcherState *ds = (struct CdbDispatcherState *)
+                                         palloc0(sizeof(struct CdbDispatcherState));
+
+        struct QueryDesc *queryDesc = (struct QueryDesc *)
+                                        palloc0(sizeof(QueryDesc));
+
+        _init_cdbdisp_dispatchPlan(queryDesc);
+
+	/* Set max plan to a value that will require handling INT32 
+	 * overflow of the current plan size */
+	gp_max_plan_size = INT_MAX;
+
+	queryDesc->plannedstmt->planTree = (struct Plan *)palloc0(sizeof(struct Plan));
+
+	/* Set num_slices and uncompressed_size to be INT_MAX-1 to force overflow */
+	queryDesc->plannedstmt->planTree->nMotionNodes = INT_MAX-1;
+
+        expect_any(serializeNode, node);
+        expect_any(serializeNode, size);
+        expect_any(serializeNode, uncompressed_size_out);
+        will_assign_value(serializeNode, uncompressed_size_out, INT_MAX-1);
+        will_return(serializeNode, NULL);
+
+        PG_TRY();
+        {
+                cdbdisp_dispatchPlan(queryDesc, true, true, ds);
+        }
+        PG_CATCH();
+        {
+		/* Verify that we get the correct error (limit exceeded) */
+                ErrorData *edata = CopyErrorData();
+
+                StringInfo message = makeStringInfo();
+                appendStringInfo(message,
+			"Query plan size limit exceeded, current size: "  UINT64_FORMAT "KB, max allowed size: %dKB", 
+				((INT_MAX-1)*(INT_MAX-1)/(uint64)1024), INT_MAX);
+
+                if ( edata->elevel == ERROR &&
+                     strncmp(edata->message, message->data, message->len))
+                {
+                        success = true;
+                }
+
+        }
+        PG_END_TRY();
+
+        assert_true(success);
+}
+	
+int		
+main(int argc, char* argv[]) {
+        cmockery_parse_arguments(argc, argv);
+
+        const UnitTest tests[] = { 
+			unit_test(test__cdbdisp_dispatchPlan__Overflow_plan_size_in_kb)
+        };  
+        return run_tests(tests);
+}
+
