e4540f6493e3e94cd4342b2ea60eed251d8a4a8f
Ed Espino <Edward.Espino@emc.com>
2013-04-25 19:45:02 -0800

    Spring cleaning. [JIRA: GPSQL-327] [git-p4: depot-paths = "//gpsql/main/": change = 161677]

--- installer/bizgrespath/mac_bizgres_path.sh
+++
@@ -1,9 +0,0 @@
-BIZHOME=/usr/local/bizgres
-PATH=$BIZHOME/pgsql/bin:$BIZHOME/client/loader/bin:$PATH
-DYLD_LIBRARY_PATH=$BIZHOME/pgsql/lib
-MANPATH=$BIZHOME/doc:$MANPATH
-
-export BIZHOME
-export PATH
-export DYLD_LIBRARY_PATH
-export MANPATH
