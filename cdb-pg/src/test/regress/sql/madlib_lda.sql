3962822e00c5de36be21846c1b09d13b897fc926
Ed Espino <Edward.Espino@emc.com>
2013-07-23 13:41:00 -0800

    Back out changelist 171481 [JIRA: GPSQL-713]
    [git-p4: depot-paths = "//gpsql/main/": change = 171608]

--- cdb-pg/src/test/regress/sql/madlib_lda.sql
+++ cdb-pg/src/test/regress/sql/madlib_lda.sql
@@ -68,8 +68,6 @@ SELECT MADLib.lda_predict(
     'lda_model', 
     'lda_pred');
 
-SELECT (lda_get_perplexity('lda_model', 'lda_pred') - 18.6155002539)^2 < 1;
-
 SELECT MADLib.lda_get_topic_desc(
     'lda_model', 
     'lda_vocab', 
