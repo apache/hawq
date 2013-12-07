0436c3a8c2f726d89d062ebb5347d43135ea1b29
Asim R Praveen <apraveen@gopivotal.com>
2013-12-06 16:38:22 -0800

    Adding set functions samples from Madlib to set funtions ICG.[JIRA: GPSQL-1408] (cherry picked from commit e34d13911e6b5954bc87267982c3416efc23a5ab)

--- cdb-pg/src/test/regress/sql/set_functions.sql
+++ cdb-pg/src/test/regress/sql/set_functions.sql
@@ -1,3 +1,9 @@
+-- start_ignore
+drop schema set_functions cascade;
+-- end_ignore
+create schema set_functions;
+set search_path=set_functions;
+
 SELECT name, setting FROM pg_settings WHERE name LIKE 'enable%';
 
 CREATE TABLE foo2(fooid int, f2 int);
@@ -148,3 +154,65 @@ DROP FUNCTION foorescan(int);
 DROP TABLE foorescan;
 DROP TABLE barrescan;
 
+--
+-- Set functions samples from Madlib
+--
+create function combination(s text) returns setof text[] as $$
+x = s.split(',')
+
+def subset(myset, N):
+   left = []
+   right = []
+   for i in range(0, len(myset)):
+      if ((1 << i) & N) > 0:
+         left.append(myset[i])
+      else:
+         right.append(myset[i])
+   return (', '.join(left), ', '.join(right))
+
+for i in range(1, (1 << len(x)) - 2):
+   yield subset(x, i)
+$$ language plpythonu strict;
+
+select x[1] || ' => ' || x[2] from combination('a,b,c,d') x;
+
+CREATE TABLE rules(rule text) distributed by (rule);
+insert into rules values('a,b,c');
+insert into rules values('d,e');
+insert into rules values('f,g,h,i,j');
+insert into rules values('k,l,m');
+
+SELECT rule, combination(rule) from rules order by 1,2;
+
+DROP TABLE IF EXISTS foo;
+CREATE TABLE foo AS SELECT rule, combination(rule) from rules distributed by (rule);
+
+
+-- UDT as argument/return type of set returning UDF
+CREATE TYPE r_type as (a int, b text);
+
+CREATE FUNCTION f1(x r_type) returns setof text as $$ SELECT $1.b from generate_series(1, $1.a) $$ language sql;
+CREATE FUNCTION f2(x int) returns setof r_type as $$ SELECT i, 'hello'::text from generate_series(1, $1) i $$ language sql;
+CREATE FUNCTION f3(x r_type) returns setof r_type as $$ SELECT $1 from generate_series(1, $1.a) $$ language sql;
+
+SELECT f1(row(2, 'hello'));
+SELECT f2(2);
+SELECT f3(row(2,'hello'));
+
+SELECT * FROM f1(row(2,'hello'));
+SELECT * FROM f2(2);
+SELECT * FROM f3(row(2,'hello'));
+
+CREATE TABLE t1 as SELECT i from generate_series(1,5) i distributed by (i);
+
+SELECT i, f1(row(i, 'hello')) from t1;
+SELECT i, f2(i) from t1;
+SELECT i, f3(row(i,'hello')) from t1;
+
+CREATE TABLE o1 as SELECT f1(row(i, 'hello')) from t1;
+CREATE TABLE o2 as SELECT f2(i) from t1;
+CREATE TABLE o3 as SELECT f3(row(i,'hello')) from t1;
+
+-- start_ignore
+drop schema set_functions cascade;
+-- end_ignore
