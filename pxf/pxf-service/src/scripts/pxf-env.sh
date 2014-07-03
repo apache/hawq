3f6c3611e47236cf777f2646e53c90514a5651f1
Dov Dorin <dov.dorin@gopivotal.com>
2014-07-03 13:32:09 +0300

    [JIRA: GPSQL-2328] PXF service LD_LIBRARY_PATH to include hadoop native libraries

--- pxf/pxf-service/src/scripts/pxf-env.sh
+++ pxf/pxf-service/src/scripts/pxf-env.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# Path to HDFS native libraries
+export LD_LIBRARY_PATH=/usr/lib/gphd/hadoop/lib/native:${LD_LIBRARY_PATH}
+
+# Path to JAVA
 export JAVA_HOME=${JAVA_HOME:=/usr/java/default}
+
