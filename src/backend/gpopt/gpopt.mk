060161b749cc557bdbc5632eece47185d7a0c765
Zhanwei Wang <wangzw@apache.org>
2015-09-28 10:54:53 +0800

    HAWQ-9. fix the Makefile in gpopt subdirectory.

--- src/backend/gpopt/gpopt.mk
+++ src/backend/gpopt/gpopt.mk
@@ -13,6 +13,14 @@ UNAME_P = $(shell uname -p)
 ARCH_OS = GPOS_$(UNAME)
 ARCH_CPU = GPOS_$(UNAME_P)
 
+ifeq (Darwin, $(UNAME))
+        LDSFX = dylib
+else
+        LDSFX = so
+endif
+
+BLD_TYPE=opt
+
 GPOPT_flags = -g3 -DGPOS_DEBUG
 ifeq "$(BLD_TYPE)" "opt"
 	GPOPT_flags = -O3 -fno-omit-frame-pointer -g3
@@ -32,11 +40,11 @@ endif
 BLD_FLAGS = $(ARCH_FLAGS) -D$(ARCH_BIT) -D$(ARCH_CPU) -D$(ARCH_OS) $(GPOPT_flags)
 override CPPFLAGS := -fPIC $(CPPFLAGS)
 override CPPFLAGS := $(BLD_FLAGS)  $(CPPFLAGS)
-override CPPFLAGS := -DGPOS_VERSION=\"$(LIBGPOS_VER)\" $(CPPFLAGS)
-override CPPFLAGS := -DGPOPT_VERSION=\"$(OPTIMIZER_VER)\" $(CPPFLAGS)
-override CPPFLAGS := -DXERCES_VERSION=\"$(XERCES_VER)\" $(CPPFLAGS)
-override CPPFLAGS := -I $(XERCES)/include $(CPPFLAGS)
-override CPPFLAGS := -I $(LIBGPOS)/include $(CPPFLAGS)
-override CPPFLAGS := -I $(OPTIMIZER)/libgpopt/include $(CPPFLAGS)
-override CPPFLAGS := -I $(OPTIMIZER)/libnaucrates/include $(CPPFLAGS)
-override CPPFLAGS := -I $(OPTIMIZER)/libgpdbcost/include $(CPPFLAGS)
+override CPPFLAGS := -DGPOS_VERSION=\"$(ORCA_DEPENDS_LIBGPOS_VER)\" $(CPPFLAGS)
+override CPPFLAGS := -DGPOPT_VERSION=\"$(ORCA_DEPENDS_OPTIMIZER_VER)\" $(CPPFLAGS)
+override CPPFLAGS := -DXERCES_VERSION=\"$(ORCA_DEPENDS_XERCES_VER)\" $(CPPFLAGS)
+override CPPFLAGS := -I $(ORCA_DEPENDS_DIR_INTER)/include $(CPPFLAGS)
+override CPPFLAGS := -I $(ORCA_DEPENDS_DIR_INTER)/libgpos/include $(CPPFLAGS)
+override CPPFLAGS := -I $(ORCA_DEPENDS_DIR_INTER)/libgpopt/include $(CPPFLAGS)
+override CPPFLAGS := -I $(ORCA_DEPENDS_DIR_INTER)/libnaucrates/include $(CPPFLAGS)
+override CPPFLAGS := -I $(ORCA_DEPENDS_DIR_INTER)/libgpdbcost/include $(CPPFLAGS)
