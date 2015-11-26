33fc5680055b2f86f5047c634769db02632720df
Wen Lin <wlin@pivotal.io>
2015-11-26 19:06:10 +0800

    HAWQ-184. Add license header to source files(part 2)

--- tools/bin/gppylib/operations/dump_analyzedb.py
+++ tools/bin/gppylib/operations/dump_analyzedb.py
@@ -1,3 +1,19 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 #####################################################################
 # This is a snapshot of dump.py in 4.2 main at March 5, 2015.
 # It is used solely for analyzedb as part of the porting of analyzedb
