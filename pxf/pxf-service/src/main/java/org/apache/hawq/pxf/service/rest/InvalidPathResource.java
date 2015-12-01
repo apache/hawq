89c41fa625623e536e1d0cc65d62b433938dcc71
Noa Horn <nhorn@pivotal.io>
2015-11-30 18:30:23 -0800

    HAWQ-179. New PXF resource to return its protocol version
    The path is <pxf host and port>/pxf/ProtocolVersion
    Output is "PXF protocol version <version>"

--- pxf/pxf-service/src/main/java/org/apache/hawq/pxf/service/rest/InvalidPathResource.java
+++ pxf/pxf-service/src/main/java/org/apache/hawq/pxf/service/rest/InvalidPathResource.java
@@ -8,9 +8,9 @@ package org.apache.hawq.pxf.service.rest;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -32,10 +32,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriInfo;
-
-class Version {
-    final static String PXF_PROTOCOL_VERSION = "v14";
-}
 
 /**
  * Class for catching paths that are not defined by other resources.
