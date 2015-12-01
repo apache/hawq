8268e669ce72dbf2354b43f9cdd7a0a4025d4648
Noa Horn <nhorn@pivotal.io>
2015-11-30 18:44:26 -0800

    HAWQ-120. Fix typo in error message

--- pxf/pxf-service/src/main/java/org/apache/hawq/pxf/service/utilities/Utilities.java
+++ pxf/pxf-service/src/main/java/org/apache/hawq/pxf/service/utilities/Utilities.java
@@ -8,9 +8,9 @@ package org.apache.hawq.pxf.service.utilities;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -61,7 +61,7 @@ public class Utilities {
                 throw new Exception(
                         "Class "
                                 + className
-                                + " doesn't not appear in classpath. "
+                                + " does not appear in classpath. "
                                 + "Plugins provided by PXF must start with \"org.apache.hawq.pxf\"",
                         e.getCause());
             } else {
--- pxf/pxf-service/src/test/java/org/apache/hawq/pxf/service/utilities/UtilitiesTest.java
+++ pxf/pxf-service/src/test/java/org/apache/hawq/pxf/service/utilities/UtilitiesTest.java
@@ -8,9 +8,9 @@ package org.apache.hawq.pxf.service.utilities;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -86,7 +86,7 @@ public class UtilitiesTest {
             assertEquals(e.getClass(), Exception.class);
             assertEquals(
                     e.getMessage(),
-                    "Class " + className + " doesn't not appear in classpath. "
+                    "Class " + className + " does not appear in classpath. "
                     + "Plugins provided by PXF must start with \"org.apache.hawq.pxf\"");
         }
     }
