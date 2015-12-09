b0972d7e5910f0bd29afe42a206645235ded7266
hubertzhang <hzhang@pivotal.io>
2015-12-09 14:28:31 +0800

    HAWQ-236. Meta data hit ratio is -0.0 when file len is zero.

--- src/backend/cdb/cdbdatalocality.c
+++ src/backend/cdb/cdbdatalocality.c
@@ -1095,7 +1095,7 @@ static void AOGetSegFileDataLocation(Relation relation,
 		fclose(fpaoseg);
 		for (int i = 0; i < fileCount; i++) {
 			BlockLocation *locations = NULL;
-			int block_num;
+			int block_num =0;
 			Relation_File *file;
 
 			int segno = i + 1;
@@ -1110,7 +1110,7 @@ static void AOGetSegFileDataLocation(Relation relation,
 
 			if (!context->keep_hash || !isRelationHash) {
 				FormatAOSegmentFileName(basepath, segno, -1, 0, &segno, segfile_path);
-				double hit_ratio;
+				double hit_ratio=0.0;
 				locations = fetch_hdfs_data_block_location(segfile_path, logic_len,
 						&block_num, relation->rd_node, segno, &hit_ratio);
 				*allblocks += block_num;
@@ -2659,6 +2659,7 @@ static void allocate_random_relation(Relation_Data* rel_data,
 
 
 	/*find the insert node for each block*/
+	uint64_t before_run_find_insert_host = gettime_microsec();
 	int *hostOccurTimes = (int *) palloc(sizeof(int) * context->dds_context.size);
 	for (int fi = 0; fi < fileCount; fi++) {
 		Relation_File *rel_file = file_vector[fi];
@@ -2701,6 +2702,12 @@ static void allocate_random_relation(Relation_Data* rel_data,
 		}
 	}
 	pfree(hostOccurTimes);
+
+	uint64_t end_run_find_insert_host = gettime_microsec();
+	int run_find_insert_host = end_run_find_insert_host - before_run_find_insert_host;
+	if(debug_datalocality_time){
+		elog(LOG, "find insert host time: %d us. \n", run_find_insert_host);
+	}
 
 	/*three stage allocation algorithm*/
 	for (int fi = 0; fi < fileCount; fi++) {
