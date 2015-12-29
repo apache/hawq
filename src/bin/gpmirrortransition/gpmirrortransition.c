ca23b22be5fb9fe15a28697cbcd0e07e9729d0ce
Ruilong Huo <rhuo@pivotal.io>
2015-12-29 00:39:15 -0800

    HAWQ-288. Support hawqfaultinjector as an internal utility for fault injection purpose - client side

--- src/bin/gpmirrortransition/gpmirrortransition.c
+++ src/bin/gpmirrortransition/gpmirrortransition.c
@@ -1,10 +1,25 @@
-/**
- * Utility to contact a segment and issue a primary/mirror mode transition
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Copyright (c) 2009 Greenplum, Inc.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
+/*
+ * Utility to contact a segment and issue a primary/mirror mode transition
+ */
 #include "postmaster/primary_mirror_mode.h"
 #include "postmaster/primary_mirror_transition_client.h"
 #include <unistd.h>
