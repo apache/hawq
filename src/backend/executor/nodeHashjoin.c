ca4111fc70b1d6ee20db90522c6114702cf496ce
hubertzhang <hzhang@pivotal.io>
2016-02-23 14:58:54 +0800

    HAWQ-438. EagerlyReleased hash table involved in hash join in explain statement introduce core on hawq dbg build.

--- src/backend/executor/nodeHashjoin.c
+++ src/backend/executor/nodeHashjoin.c
@@ -359,14 +359,14 @@ ExecHashJoin(HashJoinState *node)
 			 * If the inner relation is completely empty, and we're not doing an
 			 * outer join, we can quit without scanning the outer relation.
 			 */
-			if (TupIsNull(outerTupleSlot)
-					&& node->js.jointype != JOIN_LEFT
-					&& node->js.jointype != JOIN_LASJ
-					&& node->js.jointype != JOIN_LASJ_NOTIN
-					&& node->hj_InnerEmpty)
+			if (TupIsNull(outerTupleSlot))
 			{
 				/* end of join */
-				if (gp_eager_hashtable_release)
+				if (gp_eager_hashtable_release
+						&& node->js.jointype != JOIN_LEFT
+						&& node->js.jointype != JOIN_LASJ
+						&& node->js.jointype != JOIN_LASJ_NOTIN
+						&& node->hj_InnerEmpty)
 				{
 					ExecEagerFreeHashJoin(node);
 				}
