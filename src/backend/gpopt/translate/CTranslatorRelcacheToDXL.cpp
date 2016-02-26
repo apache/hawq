c398ceb2e94978c561b9ec65fdda1000520a1caf
Ming LI <mli@pivotal.io>
2016-02-26 14:07:17 +0800

    Revert "HAWQ-404. Add sort during INSERT of append only row oriented partition tables"
    This reverts commit 321f7e2b737baf6fe674bd0ebc0ea05f9b166897.

--- src/backend/gpopt/translate/CTranslatorRelcacheToDXL.cpp
+++ src/backend/gpopt/translate/CTranslatorRelcacheToDXL.cpp
@@ -597,7 +597,6 @@ CTranslatorRelcacheToDXL::Pmdrel
 	DrgPmdid *pdrgpmdidIndexes = NULL;
 	DrgPmdid *pdrgpmdidTriggers = NULL;
 	DrgPul *pdrgpulPartKeys = NULL;
-	ULONG ulLeafPartitions = 0;
 	BOOL fConvertHashToRandom = false;
 	DrgPdrgPul *pdrgpdrgpulKeys = NULL;
 	DrgPmdid *pdrgpmdidCheckConstraints = NULL;
@@ -653,13 +652,6 @@ CTranslatorRelcacheToDXL::Pmdrel
 				erelstorage = IMDRelation::ErelstorageAppendOnlyParquet;
 			}
 		}
-
-		// get number of leaf partitions
-		if (gpdb::FRelPartIsRoot(oid))
-		{
-		   ulLeafPartitions = gpdb::UlLeafPartitions(oid);
-		}
-
 		// get key sets
 		BOOL fAddDefaultKeys = FHasSystemColumns(rel->rd_rel->relkind);
 		pdrgpdrgpulKeys = PdrgpdrgpulKeys(pmp, oid, fAddDefaultKeys, fPartitioned, pulAttnoMapping);
@@ -730,7 +722,6 @@ CTranslatorRelcacheToDXL::Pmdrel
 							pdrgpmdcol,
 							pdrpulDistrCols,
 							pdrgpulPartKeys,
-							ulLeafPartitions,
 							fConvertHashToRandom,
 							pdrgpdrgpulKeys,
 							pdrgpmdidIndexes,
