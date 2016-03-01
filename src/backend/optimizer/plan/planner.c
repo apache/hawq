174f8177e813e2dee0613d25b850eb3bdd1c5e5f
hubertzhang <hzhang@pivotal.io>
2016-03-01 14:12:01 +0800

    Revert "Revert "HAWQ-453. Do not allocate query resource in prepare stage for prepared statement""
    This reverts commit b8b9a0e843dad2ac1e413d184979b463f5d5b4b2.

--- src/backend/optimizer/plan/planner.c
+++ src/backend/optimizer/plan/planner.c
@@ -495,7 +495,8 @@ resource_negotiator(Query *parse, int cursorOptions, ParamListInfo boundParams,
        */
       allocResult = calculate_planner_segment_num(my_parse, resourceLife,
                                           plannedstmt->rtable, plannedstmt->intoPolicy,
-                                          plannedstmt->nMotionNodes + plannedstmt->nInitPlans + 1);
+                                          plannedstmt->nMotionNodes + plannedstmt->nInitPlans + 1,
+                                          -1);
 
       Assert(allocResult);
 
