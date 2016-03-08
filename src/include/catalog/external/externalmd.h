ecb85e5c9d9a8c330b6b87380c436cabdd259e99
Oleksandr Diachenko <odiachenko@pivotal.io>
2016-03-07 17:00:00 -0800

    HAWQ-465. Implement stored procedure to return fields metainfo from PXF.

--- src/include/catalog/external/externalmd.h
+++ src/include/catalog/external/externalmd.h
@@ -17,52 +17,19 @@
  * under the License.
  */
 /*
- * hcatalogmd.h
+ * externalmd.h
  *
- *  Representation of hcatalog table metadata
  *  Created on: Mar 6, 2015
  *      Author: antova
  */
 
-#ifndef HCATALOGMD_H_
-#define HCATALOGMD_H_
+#ifndef EXTERNALMD_H_
+#define EXTERNALMD_H_
 
+#include "itemmd.h"
 #include "postgres.h"
 #include "nodes/parsenodes.h"
 
-/*
- * Metadata for columns in HCatalog tables
- */
-typedef struct HCatalogColumn
-{
-	/* column name */
-	char *colName;
-	
-	/* type name */
-	char *typeName;
-	
-	/* type modifiers, e.g. max length or precision */
-	int typeModifiers[2];
-	
-	/* number of type modifiers */
-	int nTypeModifiers;
-} HCatalogColumn;
+extern List *ParsePxfEntries(StringInfo json, char *profile, Oid dboid);
 
-/*
- * Metadata for HCatalog tables
- */
-typedef struct HCatalogTable
-{
-	/* database name */
-	char *dbName;
-
-	/* table name */
-	char *tableName;
-	
-	/* columns */
-	List *columns;
-} HCatalogTable;
-
-
-
-#endif /* HCATALOGMD_H_ */
+#endif /* EXTERNALMD_H_ */
