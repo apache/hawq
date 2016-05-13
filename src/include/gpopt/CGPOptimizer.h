2c8a8c2b16f5ac7a96e5d774d6d7fce0b8e55a39
Shreedhar Hardikar <shardikar@pivotal.io>
2016-05-12 18:21:29 -0700

    Explicitly initialize GPOPT and its dependencies.
        These were initialized by constructors earlier. To pass any parameters
        for initializing GPOPT or any of its dependendencies, we need to do that
        explicitly.

--- src/include/gpopt/CGPOptimizer.h
+++ src/include/gpopt/CGPOptimizer.h
@@ -57,6 +57,13 @@ class CGPOptimizer
 		// serialize planned statement into DXL
 		static
 		char *SzDXLPlan(Query *pquery);
+
+		// gpopt initialize and terminate
+		static
+		void InitGPOPT();
+
+		static
+		void TerminateGPOPT();
 };
 
 #endif // CGPOptimizer_H
