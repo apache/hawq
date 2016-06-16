0e62247f3822dd2101d47d47990e8fb00929b4df
Paul Guo <paulguo@gmail.com>
2016-06-16 11:10:40 +0800

    HAWQ-824. Move orca submodules to depends/
    This patch also optmized the makefiles for gpopt/gpora/gpos/gp-xerces.

--- src/backend/gpopt/gpopt.mk
+++ src/backend/gpopt/gpopt.mk
@@ -60,10 +60,12 @@ XERCES_VER  = $(shell grep "\"xerces-c\""   $(GREP_SED_VAR))
 LIBGPOS_VER = $(shell grep "\"libgpos\""    $(GREP_SED_VAR))
 OPTIMIZER_VER = $(shell grep "\"optimizer\""  $(GREP_SED_VAR))
 
-LIBGPOS_DIR = $(abs_top_builddir)/src/backend/gpos/build/install/$(prefix)
-LIBXERCES_DIR = $(abs_top_builddir)/src/backend/gp-xerces/build/install/$(prefix)
-LIBNAUCRATES_DIR = $(abs_top_builddir)/src/backend/gporca/build/install/$(prefix)
-LIBGPDBCOST_DIR = $(abs_top_builddir)/src/backend/gporca/build/install/$(prefix)
+ORCA_BLD_PATH=$(abs_top_builddir)/depends/thirdparty
+
+LIBGPOS_DIR = $(ORCA_BLD_PATH)/gpos/build/install/$(prefix)
+LIBXERCES_DIR = $(ORCA_BLD_PATH)/gp-xerces/build/install/$(prefix)
+LIBNAUCRATES_DIR = $(ORCA_BLD_PATH)/gporca/build/install/$(prefix)
+LIBGPDBCOST_DIR = $(ORCA_BLD_PATH)/gporca/build/install/$(prefix)
 LIBGPOPT_DIR = $(abs_top_builddir)/src/backend/gpopt/build/install/$(prefix)
 
 BLD_FLAGS = $(ARCH_FLAGS) -D$(ARCH_BIT) -D$(ARCH_CPU) -D$(ARCH_OS) $(GPOPT_flags)
