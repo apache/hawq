2b940258f0533dc4c7cf82b7a2117ddb3040d529
Wen Lin <wlin@pivotal.io>
2016-07-11 15:40:21 +0800

    HAWQ-912. Skip temporary Directories checking for master/standby

--- src/backend/resourcemanager/resourcemanager.c
+++ src/backend/resourcemanager/resourcemanager.c
@@ -1048,14 +1048,17 @@ int  loadDynamicResourceManagerConfigure(void)
 				 DRMGlobalInstance->SegmentMemoryMB,
 				 DRMGlobalInstance->SegmentCore);
 
-    // For temporary directories
-    InitTemporaryDirs(&DRMGlobalInstance->LocalHostTempDirectories, rm_seg_tmp_dirs);
+	if ( DRMGlobalInstance->Role == START_RM_ROLE_SEGMENT )
+	{
+		// For temporary directories
+		InitTemporaryDirs(&DRMGlobalInstance->LocalHostTempDirectories, rm_seg_tmp_dirs);
 
-	DQUEUE_LOOP_BEGIN(&DRMGlobalInstance->LocalHostTempDirectories, iter, SimpStringPtr, value)
-		elog(LOG, "HAWQ Segment RM :: Temporary directory %s", value->Str);
-	DQUEUE_LOOP_END
+		DQUEUE_LOOP_BEGIN(&DRMGlobalInstance->LocalHostTempDirectories, iter, SimpStringPtr, value)
+			elog(LOG, "HAWQ Segment RM :: Temporary directory %s", value->Str);
+		DQUEUE_LOOP_END
 
-	checkAndBuildFailedTmpDirList();
+		checkAndBuildFailedTmpDirList();
+	}
 
 	/****** Resource enforcement GUCs begins ******/
 
