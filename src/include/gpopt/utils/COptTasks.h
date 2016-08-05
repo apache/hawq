a17647be029a42c85e659cfc7fe253708a036bd7
Haisheng Yuan <hyuan@pivotal.io>
2016-08-05 15:37:33 -0500

    HAWQ-934. Populate canSetTag of PlannedStmt from Query object
    HAWQ generated an error if a single query resulted in multiple query plans
    because of rule transformation and the plans were produced by PQO. This is
    because of an incorrect directive in the plan to lock the same resource
    more than once.
    This issue has been fixed.

--- src/include/gpopt/utils/COptTasks.h
+++ src/include/gpopt/utils/COptTasks.h
@@ -221,7 +221,7 @@ class COptTasks
 
 		// translate a DXL tree into a planned statement
 		static
-		PlannedStmt *Pplstmt(IMemoryPool *pmp, CMDAccessor *pmda, const CDXLNode *pdxln);
+		PlannedStmt *Pplstmt(IMemoryPool *pmp, CMDAccessor *pmda, const CDXLNode *pdxln, bool canSetTag);
 
 		// load search strategy from given path
 		static
