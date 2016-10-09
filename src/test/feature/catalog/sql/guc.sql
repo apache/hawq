28efc7403de179a7737edee577fac25b1c2baeb9
Paul Guo <paulguo@gmail.com>
2016-10-09 17:47:32 +0800

    HAWQ-1092. lc_collate and lc_ctype do not work after setting through hawq init

--- src/test/feature/catalog/sql/guc.sql
+++ src/test/feature/catalog/sql/guc.sql
@@ -9,3 +9,5 @@ SELECT f1 FROM DATE_TBL;
 SET DATESTYLE TO 'POSTGRES, DMY';
 SELECT f1 FROM DATE_TBL;
 
+--- Partially test JIRA HAWQ-1092 lc_collate and lc_ctype do not work after setting through hawq init
+SELECT name, setting from pg_settings where name like 'lc%'
