68867d781e138b0504145ef65c73b0d996fe5216
hzhang2 <zhanghuan929@163.com>
2016-11-03 16:21:24 +0800

    HAWQ-1143. Libhdfs create semantic is not consistent with posix standard.

--- depends/libhdfs3/src/client/Hdfs.cpp
+++ depends/libhdfs3/src/client/Hdfs.cpp
@@ -646,9 +646,9 @@ hdfsFile hdfsOpenFile(hdfsFS fs, const char * path, int flags, int bufferSize,
         if ((flags & O_CREAT) || (flags & O_APPEND) || (flags & O_WRONLY)) {
             int internalFlags = 0;
 
-            if (flags & O_CREAT) {
+            if ((flags & O_CREAT) && (flags & O_EXCL)) {
                 internalFlags |= Hdfs::Create;
-            } else if ((flags & O_APPEND) && (flags & O_WRONLY)) {
+            } else if ((flags & O_CREAT) || ((flags & O_APPEND) && (flags & O_WRONLY))) {
                 internalFlags |= Hdfs::Create;
                 internalFlags |= Hdfs::Append;
             } else if (flags & O_WRONLY) {
