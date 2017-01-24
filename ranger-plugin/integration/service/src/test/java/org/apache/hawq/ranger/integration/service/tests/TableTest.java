dc6ecd6cf390db9e7ebde49b5cbc263cb065dd4c
Alexander Denissov <adenissov@pivotal.io>
2017-01-24 11:47:16 -0800

    TableTest

---
+++ ranger-plugin/integration/service/src/test/java/org/apache/hawq/ranger/integration/service/tests/TableTest.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.hawq.ranger.integration.service.tests;
+
+import org.apache.hawq.ranger.integration.service.tests.policy.Policy;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.apache.hawq.ranger.integration.service.tests.policy.Policy.ResourceType.*;
+
+public class TableTest extends ServiceTestBase {
+
+    private static final String TEST_DB = "test-db";
+    private static final String TEST_SCHEMA = "test-schema";
+    private static final String TEST_TABLE = "test-table";
+
+    @Before
+    public void beforeTest() {
+        specificResource.put(database, TEST_DB);
+        specificResource.put(schema, TEST_SCHEMA);
+        specificResource.put(table, TEST_TABLE);
+
+        parentUnknownResource.put(database, TEST_DB);
+        parentUnknownResource.put(schema, UNKNOWN);
+        parentUnknownResource.put(table, TEST_TABLE);
+
+        childUnknownResource.put(database, TEST_DB);
+        childUnknownResource.put(schema, TEST_SCHEMA);
+        childUnknownResource.put(table, UNKNOWN);
+
+        unknownResource.put(database, UNKNOWN);
+        unknownResource.put(schema, UNKNOWN);
+        unknownResource.put(table, UNKNOWN);
+
+        privileges = new String[] {"select", "insert", "update", "delete", "references"};
+    }
+
+    @Override
+    protected Policy getResourceUserPolicy() {
+        Policy policy = policyBuilder
+                .resource(database, TEST_DB)
+                .resource(schema, TEST_SCHEMA)
+                .resource(table, TEST_TABLE)
+                .userAccess(TEST_USER, privileges)
+                .build();
+        return policy;
+    }
+
+    protected Policy getResourceParentStarUserPolicy() {
+        Policy policy = policyBuilder
+                .resource(database, TEST_DB)
+                .resource(schema, STAR)
+                .resource(table, TEST_TABLE)
+                .userAccess(TEST_USER, privileges)
+                .build();
+        policy.isParentStar = true;
+        return policy;
+    }
+
+    protected Policy getResourceChildStarUserPolicy() {
+        Policy policy = policyBuilder
+                .resource(database, TEST_DB)
+                .resource(schema, TEST_SCHEMA)
+                .resource(table, STAR)
+                .userAccess(TEST_USER, privileges)
+                .build();
+        policy.isChildStar = true;
+        return policy;
+    }
+
+    @Override
+    protected Policy getResourceGroupPolicy() {
+        Policy policy = policyBuilder
+                .resource(database, TEST_DB)
+                .resource(schema, TEST_SCHEMA)
+                .resource(table, TEST_TABLE)
+                .groupAccess(PUBLIC_GROUP, privileges)
+                .build();
+        return policy;
+    }
+
+    @Test
+    public void testParentStarResourceUserPolicy() throws IOException {
+        checkResourceUserPolicy(getResourceParentStarUserPolicy());
+    }
+
+    @Test
+    public void testChildStarResourceUserPolicy() throws IOException {
+        checkResourceUserPolicy(getResourceChildStarUserPolicy());
+    }
+}
