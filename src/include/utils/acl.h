8100a9c5ea66333cf595bf529ee18ee9e964228a
chunlingwang and hubertzhang <pair+chunlingwang+hubertzhang@apache.org>
2017-02-23 12:59:47 +0800

    HAWQ-1357. Super user also need to check create privilege of public schema from Ranger.

--- src/include/utils/acl.h
+++ src/include/utils/acl.h
@@ -316,7 +316,7 @@ extern List *pg_rangercheck_batch(List *);
 extern AclResult
 pg_rangercheck(AclObjectKind objkind, Oid table_oid, Oid roleid,
          AclMode mask, AclMaskHow how);
-extern bool fallBackToNativeCheck(AclObjectKind objkind, Oid table_oid, Oid roleid);
+extern bool fallBackToNativeCheck(AclObjectKind objkind, Oid table_oid, Oid roleid, AclMode mode);
 extern bool fallBackToNativeChecks(AclObjectKind objkind, List* table_list, Oid roleid);
 extern char *getNameFromOid(AclObjectKind objkind, Oid object_oid);
 extern char *getClassNameFromOid(Oid object_oid);
