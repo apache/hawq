b3dbcc24b2e702c7a571a4a44057f2fe70c71a4d
Hubert Zhang <hubertzhang@apache.org>
2017-02-28 14:42:24 +0800

    Fix license and other matters.
    Signed-off-by: Chuling Wang <wangchunling14@126.com>

--- src/test/feature/Ranger/ans/rename.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for  i in `ls|grep out`
-do
-    newName=`echo $i | sed 's/.\out/\.ans/g'`
-    mv $i $newName
-done
--- src/test/feature/Ranger/ans/renameans.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for  i in `ls|grep ans|grep admin`
-do
-    newName=`echo $i | sed 's/admin/adminfirst/g'`
-    cp $i $newName
-done
