c024842ff3102aae0611d468da4f36aedb7e3c07
amyrazz44 <abai@pivotal.io>
2017-07-11 15:51:32 +0800

    HAWQ-1501. Support TDE by adding KmsClientProvider class to interact with KMS server.

---
+++ depends/libhdfs3/mock/MockKmsClientProvider.h
@@ -0,0 +1,50 @@
+/********************************************************************
+ * 2014 - 
+ * open source under Apache License Version 2.0
+ ********************************************************************/
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef _HDFS_LIBHDFS3_MOCK_KMSCLIENTPROVIDER_H_
+#define _HDFS_LIBHDFS3_MOCK_KMSCLIENTPROVIDER_H_
+
+#include "gmock/gmock.h"
+
+#include "client/KmsClientProvider.h"
+
+using namespace Hdfs::Internal;
+
+class MockKmsClientProvider: public Hdfs::KmsClientProvider {
+public:
+  MockKmsClientProvider(std::shared_ptr<RpcAuth> auth, std::shared_ptr<SessionConfig> conf) : KmsClientProvider(auth, conf) {}
+  MOCK_METHOD1(setHttpClient, void(std::shared_ptr<HttpClient> hc));
+  MOCK_METHOD1(getKeyMetadata, ptree(const FileEncryptionInfo &encryptionInfo));
+  MOCK_METHOD1(deleteKey, void(const FileEncryptionInfo &encryptionInfo));
+  MOCK_METHOD1(decryptEncryptedKey, ptree(const FileEncryptionInfo &encryptionInfo));
+  MOCK_METHOD5(createKey, void(const std::string &keyName, const std::string &cipher, const int length, const std::string &material, const std::string &description));
+
+ ptree getEDKResult(FileEncryptionInfo &encryptionInfo) {
+    ptree map;
+    map.put("name", encryptionInfo.getKeyName());
+    map.put("iv", encryptionInfo.getIv());
+    map.put("material", KmsClientProvider::base64Encode(encryptionInfo.getKey()));
+    return map;
+  }
+
+};
+
+#endif /* _HDFS_LIBHDFS3_MOCK_KMSCLIENTPROVIDER_H_ */
---
+++ depends/libhdfs3/src/client/KmsClientProvider.h
@@ -0,0 +1,142 @@
+/********************************************************************
+ * 2014 -
+ * open source under Apache License Version 2.0
+ ********************************************************************/
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef _HDFS_LIBHDFS3_CLIENT_KMSCLIENTPROVIDER_H_
+#define _HDFS_LIBHDFS3_CLIENT_KMSCLIENTPROVIDER_H_
+
+#include <string>
+#include <gsasl.h>
+
+#include "openssl/conf.h"
+#include "openssl/evp.h"
+#include "openssl/err.h"
+#include "FileEncryptionInfo.h"
+#include "HttpClient.h"
+#include <vector>
+#include "common/SessionConfig.h"
+#include "rpc/RpcAuth.h"
+
+#include <boost/property_tree/ptree.hpp>
+
+using boost::property_tree::ptree;
+using namespace Hdfs::Internal;
+
+
+namespace Hdfs {
+
+class KmsClientProvider {
+public:
+
+	/**
+     * Construct a KmsClientProvider instance.
+     * @param auth RpcAuth to get the auth method and user info.
+	 * @param conf a SessionConfig to get the configuration.
+     */
+	KmsClientProvider(std::shared_ptr<RpcAuth> auth, std::shared_ptr<SessionConfig> conf);
+
+	/**
+     * Destroy a KmsClientProvider instance.
+     */
+	virtual ~KmsClientProvider(){
+	}
+
+	/**
+	 * Set HttpClient object.
+	 */
+	void setHttpClient(std::shared_ptr<HttpClient> hc);
+
+	/**
+	 * Create an encryption key from kms.
+	 * @param keyName the name of this key.
+	 * @param cipher the ciphertext of this key. e.g. "AES/CTR/NoPadding" .
+	 * @param length the length of this key.
+	 * @param material will be encode to base64.
+	 * @param description key's info.
+ 	*/	
+	virtual void createKey(const std::string &keyName, const std::string &cipher, const int length, const std::string &material, const std::string &description);
+
+	/**
+	 * Get key metadata based on encrypted file's key name.
+	 * @param encryptionInfo the encryption info of file.
+ 	* @return return response info about key metadata from kms server.
+	 */
+	virtual ptree getKeyMetadata(const FileEncryptionInfo &encryptionInfo);
+
+	/**
+ 	 * Delete an encryption key from kms.
+	 * @param encryptionInfo the encryption info of file.
+	 */
+	virtual void deleteKey(const FileEncryptionInfo &encryptionInfo);
+
+	/**
+ 	 * Decrypt an encrypted key from kms.
+	 * @param encryptionInfo the encryption info of file.
+	 * @return return decrypted key.
+	 */
+	virtual ptree decryptEncryptedKey(const FileEncryptionInfo &encryptionInfo);
+
+	/**
+	 * Encode string to base64.
+	 */
+	static std::string	base64Encode(const std::string &data);
+
+	/**
+	 * Decode base64 to string.
+	 */	
+	static std::string	base64Decode(const std::string &data);
+
+private:
+
+	/**
+	 * Convert ptree format to json format.
+	 */
+	static std::string  toJson(const ptree &data);
+
+	/**
+ 	 * Convert json format to ptree format.
+	 */
+	static ptree		fromJson(const std::string &data);
+
+	/**
+	 * Parse kms url from configure file.
+	 */
+	std::string 		parseKmsUrl();
+
+	/**
+	 * Build kms url based on urlSuffix and different auth method.
+	 */
+	std::string 		buildKmsUrl(const std::string &url, const std::string &urlSuffix);
+	/**
+ 	 * Set common headers for kms API.
+ 	 */
+	void setCommonHeaders(std::vector<std::string>& headers);	
+
+	std::shared_ptr<HttpClient> 	hc;
+	std::string						url;
+
+	std::shared_ptr<RpcAuth> 		auth;
+	AuthMethod						method;
+	std::shared_ptr<SessionConfig> 	conf;
+	
+};
+
+}
+#endif
