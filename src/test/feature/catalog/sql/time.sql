5e152951d9a648a797199f2d21513bb8e30e4644
Kuien Liu <kuien.lke@alibaba-inc.com>
2017-11-30 17:43:12 +0800

    HAWQ-1532. test cases on 'Asia' timezone

--- src/test/feature/catalog/sql/time.sql
+++ src/test/feature/catalog/sql/time.sql
@@ -39,3 +39,28 @@ SELECT f1 AS "Eight" FROM TIME_TBL WHERE f1 >= '00:00' ORDER BY 1;
 -- where we do mixed-type arithmetic. - thomas 2000-12-02
 
 SELECT f1 + time '00:01' AS "Illegal" FROM TIME_TBL;
+
+--
+-- Support timezone_abbreviations = 'Asia'
+--
+-- For some reason this seems to have been missed to support
+-- CST (China Standard Time). On some platforms, for example,
+-- Linux, CST is used as time string suffix of GMT+8, especially
+-- for users in China. In such case, the timestamp of log is
+-- '14 hours' ahead of the actual China time (GMT+8).
+
+SHOW timezone_abbreviations; -- info for debug, should always be 'Default'
+
+SET timezone_abbreviations TO 'Asia';
+SELECT abbrev, utc_offset FROM pg_timezone_abbrevs WHERE abbrev = 'CST';
+SELECT '2002-09-24 10:10 CST'::TIMESTAMP WITH TIME ZONE;
+SELECT '2002-09-24 10:10 Asia/Shanghai'::TIMESTAMP WITH TIME ZONE; -- won't change
+SELECT '2002-09-24 10:10 GST'::TIMESTAMP WITH TIME ZONE; -- Gulf Standard Time, Asia/Dubai
+SELECT '2002-09-24 10:10 IST'::TIMESTAMP WITH TIME ZONE; -- Indian Standard Time
+
+SET timezone_abbreviations TO 'Default';
+SELECT abbrev, utc_offset FROM pg_timezone_abbrevs WHERE abbrev = 'CST';
+SELECT '2002-09-24 10:10 CST'::TIMESTAMP WITH TIME ZONE;
+SELECT '2002-09-24 10:10 Asia/Shanghai'::TIMESTAMP WITH TIME ZONE; -- won't change
+SELECT '2002-09-24 10:10 GST'::TIMESTAMP WITH TIME ZONE; -- 'GST' is invalid in Default
+SELECT '2002-09-24 10:10 IST'::TIMESTAMP WITH TIME ZONE; -- Israel Standard Time
