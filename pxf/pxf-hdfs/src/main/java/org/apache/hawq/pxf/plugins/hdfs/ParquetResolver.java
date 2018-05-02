2fcaea77e8077222351a3d470e4e4e93b7901d2c
Michael Goddard <mgoddard@gopivotal.com>
2018-05-02 15:56:45 -0400

    Adding handling of DATA and TIMESTAMP_MILLIS (Avro types), which equate, respectively, to Greenplum DATE and TIMESTAMP types

--- pxf/pxf-hdfs/src/main/java/org/apache/hawq/pxf/plugins/hdfs/ParquetResolver.java
+++ pxf/pxf-hdfs/src/main/java/org/apache/hawq/pxf/plugins/hdfs/ParquetResolver.java
@@ -101,6 +101,12 @@ public class ParquetResolver extends Plugin implements ReadResolver {
                     field.type = DataType.BYTEA.getOID();
                     field.val = g.getFieldRepetitionCount(columnIndex) == 0 ?
                             null : g.getBinary(columnIndex, 0).getBytes();
+                } else if (originalType == OriginalType.DATE) { // DATE type
+                    field.type = DataType.DATE.getOID();
+                    field.val = g.getFieldRepetitionCount(columnIndex) == 0 ? null : g.getString(columnIndex, 0);
+                } else if (originalType == OriginalType.TIMESTAMP_MILLIS) { // TIMESTAMP type
+                    field.type = DataType.TIMESTAMP.getOID();
+                    field.val = g.getFieldRepetitionCount(columnIndex) == 0 ? null : g.getString(columnIndex, 0);
                 } else {
                     field.type = DataType.TEXT.getOID();
                     field.val = g.getFieldRepetitionCount(columnIndex) == 0 ?
