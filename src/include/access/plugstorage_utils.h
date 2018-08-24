ae2af74b2cf6f6679deca6f26be76bdbf7d02fa6
oushu1wangziming1 <wang.ziming@oushu.io>
2018-08-24 10:40:02 +0800

    HAWQ-1629. Add ORC format using pluggable storage framework.

--- src/include/access/plugstorage_utils.h
+++ src/include/access/plugstorage_utils.h
@@ -80,14 +80,16 @@ typedef struct PlugStorageInsertFuncs
  *    method for them for each tuple.
  *
  */
-typedef enum
-{
-	ExternalTableType_GENERIC,     /* GENERIC external table format and protocol */
-	ExternalTableType_TEXT,        /* TEXT format with gpfdist(s), http, command protocol */
-	ExternalTableType_CSV,         /* CSV format with gpfdist(s), http, command protocol */
-	ExternalTableType_CUSTOM,      /* Temporary workaround to support the old framework */
-	ExternalTableType_PLUG,        /* Pluggable format with hdfs protocol, i.e., ORC */
-	ExternalTableType_Invalid
+typedef enum {
+  ExternalTableType_GENERIC, /* GENERIC external table format and protocol */
+  ExternalTableType_TEXT, /* TEXT format with gpfdist(s), http, command protocol
+                           */
+  ExternalTableType_CSV,  /* CSV format with gpfdist(s), http, command protocol
+                           */
+  ExternalTableType_TEXT_CUSTOM, /* TEXT format with hdfs protocol */
+  ExternalTableType_CSV_CUSTOM,  /* CSV format with hdfs protocol */
+  ExternalTableType_PLUG, /* Pluggable format with hdfs protocol, i.e., ORC */
+  ExternalTableType_Invalid
 } ExternalTableType;
 
 #endif	/* PLUGSTORAGE_UTILS_H */
