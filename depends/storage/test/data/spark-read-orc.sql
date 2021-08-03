b0a71f0617b68c0977acd6d3601052a7e80bacf5
ztao1987 <zhenglin.taozl@gmail.com>
2021-08-03 11:37:05 +0800

    HAWQ-1799. delete useless codes

--- depends/storage/test/data/spark-read-orc.sql
+++
@@ -1,3 +0,0 @@
-CREATE EXTERNAL TABLE testorc (i2 short, i4 int, i8 long, f4 float, f8 double, text string) stored as orc LOCATION 'hdfs://localhost:9000/testorc' TBLPROPERTIES ("orc.bloom.filter.columns"="i4")
-select * from testorc where i4=3;
-select * from testorc where i4=4;
